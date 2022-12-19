b152e6a0b3af47f2a84e278e5d262d267744ead2
VKoshovec <116878840+VKoshovec@users.noreply.github.com>
2022-12-19 21:53:49 +0000

    Deploying to gh-pages from @ VKoshovec/teamProjJS6@09e40e831836a59ede41973c7947734bd0cb4123 🚀

---
+++ index.610f196d.js
@@ -0,0 +1,2 @@
+console.log("Hello world part1"),console.log("Hello world part2");
+//# sourceMappingURL=index.610f196d.js.map
---
+++ index.6144f50f.js
@@ -0,0 +1,2 @@
+console.log("Hello world part1"),console.log("Hello world part2");
+//# sourceMappingURL=index.6144f50f.js.map
