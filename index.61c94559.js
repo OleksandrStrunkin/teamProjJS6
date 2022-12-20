c39d0b87e1946263be69a040e63ea481bd73dcc8
VKoshovec <116878840+VKoshovec@users.noreply.github.com>
2022-12-20 07:59:57 +0000

    Deploying to gh-pages from @ VKoshovec/teamProjJS6@0a996a001364425d281274d911a11f85c680b8d6 🚀

--- index.61c94559.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world part1"),console.log(`Branch test ${Date.now()}`),console.log("Hello world part2");
-//# sourceMappingURL=index.61c94559.js.map
--- index.eac79d45.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world part1"),console.log("Branch test ".concat(Date.now())),console.log("Hello world part2");
-//# sourceMappingURL=index.eac79d45.js.map
